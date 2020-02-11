c76d18e46685f3b5337d68a359e01b1c2de3edac
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-02-11 17:04:53 -0300

    Added implementation

--- ex_01/src/index.js
+++ ex_01/src/index.js
@@ -1,3 +1,7 @@
-const { compareEncrypt, encryptPassword } = require('./actions/Cryptography');
+const UserSchema = require('./models/User');
+const { setName, setEmail, setPassword } = require('./actions/User');
+const { compareEncrypt } = require('./actions/Cryptography');
 
-console.log(encryptPassword({ password: '123456' }));
+console.log(setName(UserSchema)('foo'));
+console.log(setEmail(UserSchema)('foo@email.com'));
+console.log(setPassword(UserSchema)('123456'));
