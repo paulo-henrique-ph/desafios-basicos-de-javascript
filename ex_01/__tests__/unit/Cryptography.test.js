5b1cf3c44a740d0149910bb739817202bc96642b
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-02-11 14:26:28 -0300

    Added unit test sketch

--- ex_01/__tests__/unit/Cryptography.test.js
+++ ex_01/__tests__/unit/Cryptography.test.js
@@ -4,3 +4,8 @@ test('return a hashed string of the password', async () => {
   expect(await encryptPassword({ password: '123456' }))
     .toBe('...');
 });
+
+test('compare a password to it\'s hashed counterpart', async () => {
+  expect(await compareEncrypt('123456', '...'))
+    .toBe(true);
+});
