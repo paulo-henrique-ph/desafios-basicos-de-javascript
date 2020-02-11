14e332d32e3e7c23b8b3e0d6cb99556ee4496a0b
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-02-11 12:39:07 -0300

    Added salt function

--- ex_01/src/actions/Cryptography.js
+++ ex_01/src/actions/Cryptography.js
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt';
+import bcrypt, { genSaltSync } from 'bcrypt';
 
-const encryptPassword = async ({ password }) => bcrypt.hash(password);
+const encryptPassword = async ({ password }) => bcrypt.hash(password, genSaltSync());
 
 const compareEncrypt = async (password, hash) => bcrypt.compare(password, hash);
 
